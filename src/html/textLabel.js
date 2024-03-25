77f13ce3826000b360d8ffdfbb70131643d22ba3
Venomousquid <vincevleer@hotmail.com>
2024-03-25 22:38:44 +0100

    update

--- src/html/textLabel.js
+++
@@ -1,3 +0,0 @@
-export const textLabel = (text = 'text', id = '') => `
-	<${id ? 'a' : 'div'} class="text-label" id="${id}" ${ id ? `href="#${id}"` : '' }>${text}</${id ? 'a' : 'div'}>
-`;
--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-	root: './src/',
-	base: '/horse-staple',
-	build: {
-		outDir: '../dist'
-	},
-})
